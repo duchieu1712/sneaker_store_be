2353e870e90319c66c482dc504205a1dfec369c0
thang.lengoc <thang.lengoc@thegioididiong.com>
2022-08-16 12:09:39 +0700

    make it better

--- src/routes/v1/brandRoute.js
+++ src/routes/v1/brandRoute.js
@@ -7,7 +7,7 @@ const upload = require('../../config/upload')
 
 
 brandRoute.get("/getBrand", brandController.getBrand)
-brandRoute.post("/addBrand", brandController.addBrand)
+brandRoute.post("/addBrand",upload.single("brand"), brandController.addBrand)
 brandRoute.put("/updateBrand", brandController.updateBrand)
 brandRoute.delete("/deleteBrand", brandController.deleteBrand)
 module.exports = brandRoute
