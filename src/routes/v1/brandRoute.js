bddeb7ca60a17a5210fc42335c61a54b153f7a39
NGUYEN Duc Hieu <duchieu.nguyen@amaris.com>
2022-08-16 15:08:38 +0700

    make it better

--- src/routes/v1/brandRoute.js
+++ src/routes/v1/brandRoute.js
@@ -7,7 +7,7 @@ const upload = require('../../config/upload')
 
 
 brandRoute.get("/getBrand", brandController.getBrand)
-brandRoute.post("/addBrand",upload.array("brand"), brandController.addBrand)
+brandRoute.post("/addBrand",upload.array("brand",2), brandController.addBrand)
 brandRoute.put("/updateBrand", brandController.updateBrand)
 brandRoute.delete("/deleteBrand", brandController.deleteBrand)
 module.exports = brandRoute
