d5c5910ba73a490bc374fc96dc6461136acb5c41
thang.lengoc <thang.lengoc@thegioididiong.com>
2022-08-16 11:14:24 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const app = express();
 const config = require('../src/config')
 
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.static('.'))
 app.use(express.json());
 
