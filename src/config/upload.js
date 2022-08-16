eef222c20cbb856a0d4ff4c1894415f9248e16dd
NGUYEN Duc Hieu <duchieu.nguyen@amaris.com>
2022-08-16 14:27:59 +0700

    make it better

--- src/config/upload.js
+++ src/config/upload.js
@@ -2,8 +2,8 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        // cb(null, `${process.cwd()}/public/img`)
-        cb(null, '/src/images')
+        cb(null, `${process.cwd()}/src/images`)
+        // cb(null, '/src/images')
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + file.originalname; //60423412312+ten_img.jpg
