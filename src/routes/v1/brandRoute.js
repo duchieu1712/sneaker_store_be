85a725be35ca5a251cf7f0b255b3c178a0622e10
thang.lengoc <thang.lengoc@thegioididiong.com>
2022-08-16 11:24:44 +0700

    make it better

--- src/routes/v1/brandRoute.js
+++ src/routes/v1/brandRoute.js
@@ -3,7 +3,8 @@ const express = require('express')
 const brandRoute = express.Router()
 
 const brandController = require('../../controllers/brandController')
-const upload = require('./upload')
+const upload = require('../../config/upload')
+
 
 brandRoute.get("/getBrand", brandController.getBrand)
 brandRoute.post("/addBrand",upload.single("brand"), brandController.addBrand)
