e62aa5a0b9231827d35a9f6ef1a6fe36dbd6f76b
thang.lengoc <thang.lengoc@thegioididiong.com>
2022-08-16 12:43:15 +0700

    make it better

--- src/controllers/brandController.js
+++ src/controllers/brandController.js
@@ -16,7 +16,7 @@ const addBrand = async (req, res) => {
   try {
     // console.log(req.file.path);
     let { name, descrip } = req.body;
-    let image = `${base_url}img/${req.file?.filename}`
+    let image = `${base_url}public/img/${req.file?.filename}`
     // let image = `${req.file?.path}`
     const checkBrandExist = await model.brand.findOne({
       where: { name: name },
