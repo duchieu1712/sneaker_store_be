580cf40dedb0bb0122a95d188eb9fa99e0de4da2
NGUYEN Duc Hieu <duchieu.nguyen@amaris.com>
2022-08-23 10:43:23 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const config = require('../src/config')
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
