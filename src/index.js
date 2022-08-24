596c5c93febf08347235a99c7eb0dd775be3cabc
NGUYEN Duc Hieu <duchieu.nguyen@amaris.com>
2022-08-24 15:57:47 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const config = require('../src/config')
 
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(bodyParser.json())
+const router = express.Router();
+router.use(express.json())
+router.use(express.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
