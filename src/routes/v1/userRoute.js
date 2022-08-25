032537d9f2248717501b574f8796f1c550c44717
NGUYEN Duc Hieu <duchieu.nguyen@amaris.com>
2022-08-25 11:12:36 +0700

    make it better

--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -6,7 +6,7 @@ const authController = require('../../controllers/authController')
 userRoute.get("/getUserList", authController.verifyToken, userController.getUserList)
 userRoute.post("/signIn",userController.signIn)
 userRoute.post("/signUp",userController.signUp)
-userRoute.post("/updateUser/:id", authController.verifyToken, userController.updateUser)
+userRoute.put("/updateUser/:id", authController.verifyToken, userController.updateUser)
 userRoute.post("/deleteUser", authController.verifyToken, userController.deleteUser)
 
 module.exports = userRoute
