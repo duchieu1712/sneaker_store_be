c569a4193e8ac847c2ba31453185ce2c3cbb8367
thang.lengoc <thang.lengoc@thegioididiong.com>
2022-08-30 11:48:48 +0700

    make it better

--- src/routes/v1/deliveryRoute.js
+++ src/routes/v1/deliveryRoute.js
@@ -5,7 +5,7 @@ const deliveryRoute = express.Router()
 const deliveryController = require('../../controllers/deliveryController')
 
 deliveryRoute.get("/getDeliveries", deliveryController.getDeliveries)
-deliveryRoute.post("/addDelivery", deliveryController.addDelivery)
-deliveryRoute.put("/updateDelivery/:id", deliveryController.updateDelivery)
-deliveryRoute.post("/deleteDelivery", deliveryController.deleteDelivery)
+deliveryRoute.post("/addDelivery", authController.verifyToken, deliveryController.addDelivery)
+deliveryRoute.put("/updateDelivery/:id", authController.verifyToken, deliveryController.updateDelivery)
+deliveryRoute.post("/deleteDelivery", authController.verifyToken, deliveryController.deleteDelivery)
 module.exports = deliveryRoute
