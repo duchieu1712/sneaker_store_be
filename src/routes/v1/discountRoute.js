155feaef36f92f927089e1c8c9a666f7659ad845
thang.lengoc <thang.lengoc@thegioididiong.com>
2022-08-30 11:35:18 +0700

    make it better

--- src/routes/v1/discountRoute.js
+++ src/routes/v1/discountRoute.js
@@ -5,7 +5,7 @@ const discountRoute = express.Router()
 const discountController = require('../../controllers/discountController')
 
 discountRoute.get("/getDiscounts", discountController.getDiscounts)
-discountRoute.post("/addDiscount", discountController.addDiscount)
-discountRoute.put("/updateDiscount/:id", discountController.updateDiscount)
-discountRoute.post("/deleteDiscount", discountController.deleteDiscount)
+discountRoute.post("/addDiscount", authController.verifyToken, discountController.addDiscount)
+discountRoute.put("/updateDiscount/:id", authController.verifyToken, discountController.updateDiscount)
+discountRoute.post("/deleteDiscount", authController.verifyToken, discountController.deleteDiscount)
 module.exports = discountRoute
