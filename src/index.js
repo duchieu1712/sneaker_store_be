047e8a2ba76fea83dcd7d37ddd2ed0d2080cdecb
NGUYEN Duc Hieu <duchieu.nguyen@amaris.com>
2022-09-21 16:34:43 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const rootRouter = require('./routes/index')
 const app = express();
 const config = require('../src/config')
+const cors = require('cors')
 // const bodyParser = require('body-parser')
 
 // app.use(bodyParser.urlencoded({ extended: false }))
@@ -21,5 +22,7 @@ app.listen(config.port || 8080, () => {
 
 })
 
+
+app.use(cors())
 app.use("/api",rootRouter)
 
