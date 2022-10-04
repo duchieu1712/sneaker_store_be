2671e6a3b5679ec2923babce43b1616c0bf83153
NGUYEN Duc Hieu <duchieu.nguyen@amaris.com>
2022-10-04 17:20:05 +0700

    make it better

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -24,8 +24,8 @@ const addOrder = async (req, res) => {
       order_detail.map( async (item) => {
         const orderDetailItem = Object.assign(item, { order_id: newOrder_id });
         orderDetailModel.push(orderDetailItem);
-        // const productSize = await model.product_size.findOne({where: {product_id: item.product_id, size_id: item.size_id}})
-        // await productSize.update({amount: amount - item.amount})
+        const productSize = await model.product_size.findOne({where: {product_id: item.product_id, size_id: item.size_id}})
+        await productSize.update({amount: amount - item.amount})
       });
       const result = await model.order_detail.bulkCreate(
         orderDetailModel
